5f44e19c091d7fd144a9caf87b0399679a0b702f
Jackson G. Kaindume <seestem@merely.tech>
2022-08-03 22:07:04 +0200

    add more fields to Section trait

--- src/sections/mod.rs
+++ src/sections/mod.rs
@@ -3,8 +3,14 @@ use crate::config::Config;
 /// A section of an HTML webpage
 pub trait Section {
     /// Label used to reference the section in the .toml configuration
-    type Label;
+    type L;
+
+    /// Indicates which style should be used
+    type S;
 
     /// Create section from a .toml configuration
     fn new(config: &Config) -> Self;
+
+    /// Export section to HTML string
+    fn export() -> String;
 }
